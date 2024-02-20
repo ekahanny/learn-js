cfa8a0f4f8803589f358e37d02b89b6584e4bf4a
ekahanny <ekahnny@gmail.com>
2024-02-20 18:34:45 +0800

    menambahkan materi baru

--- DOM Manipulation/selection/app.js
+++ DOM Manipulation/selection/app.js
@@ -81,4 +81,33 @@ dari satu atribut sehingga atribut yang
 ditambahkan tsb tdk ter-override
 */
 heading.classList.add("bg-info", "border-danger", "text-white");
-heading.classList.remove("text-white");
+heading.classList.remove("bg-info", "border-danger", "text-white");
+
+// *Mengakses elemen berdasarkan parent/pembungkusnya
+const textBold = document.querySelector("b");
+textBold.parentElement; //untuk mengetahui pembungkus dari sebuah elemen
+textBold.parentElement.parentElement; //mengetahui dua pembungkus diatas textBold
+
+// *Mengakses children/elemen turunan dari sebuah elemen
+paragraf.children;
+paragraf.children[0];
+paragraf.children[8].childElementCount;
+
+// *Mencari elemen sebelum atau setelahnya
+paragraf.nextSibling;
+paragraf.previousSibling;
+
+// *Menambahkan elemen baru melalui DOM
+const newCat = document.createElement("img");
+newCat.src =
+  "https://www.purina.co.id/sites/default/files/2023-01/14%20Fakta%20Tentang%20Kucing%20yang%20Unik%20dan%20Menarik%20copy.jpg";
+document.body.appendChild(newCat); // menggunakan appendchild
+newCat.classList.add("square"); // menambahkan class agar style css berlaku pada img baru
+
+const subHeader = document.createElement("h3");
+subHeader.append("This is about cats");
+heading.insertAdjacentElement("afterend", subHeader); //menggunakan method lain
+
+// *Menghapus suatu element
+// subHeader.remove();
+// newCat.remove();
