7eb88480eafb4112e4c02227bbe617cafa8a3f70
ekahanny <ekahnny@gmail.com>
2024-02-29 14:25:01 +0800

    menambahkan materi multi thread

--- Campuscoder/Asynchronous/CallStack.js
+++ Campuscoder/Asynchronous/CallStack.js
@@ -16,3 +16,26 @@ const pythagoras = (a, b, c) => {
 };
 
 console.log(pythagoras(3, 4, 5));
+
+// SINGLE THREAD
+
+/*
+
+Pada dasarnya, JS bersifat single thread
+(single thread adalah program yang hanya
+dapat dijalankan pada satu waktu). Untuk 
+mengatasi hal tsb, kita bisa menggunakan
+prinsip multi-thread yang dikerjakan oleh
+browser engine.
+
+*/
+
+console.log("Muncul Pertama");
+
+setTimeout(() => {
+  console.log(
+    "Ini sudah memasuki memory proses tapi dijalankan setelah 3 detik"
+  );
+}, 3000);
+
+console.log("Muncul Kedua");
